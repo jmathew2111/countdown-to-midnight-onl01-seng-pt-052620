415c535a3b7afea4b5b8c983d4cbcfc55af9878e
Justin Mathew <jmathew2111@yahoo.com>
2020-06-10 01:02:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,5 +10,12 @@ end
   "HAPPY NEW YEAR!"
 end
 
-
+def countdown(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
